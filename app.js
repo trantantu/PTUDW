7fa68291dcff69becf59a04c2e98765584e9c1ef
trantantu <trantantu85@gmail.com>
2022-09-23 07:49:41 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require("cors");
-
+const contactsRouter = require("./app/routes/contact.route");
 const app = express();
 
 
@@ -10,5 +10,5 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
 });
-
+app.use("/api/contacts", contactsRouter);
 module.exports = app;
